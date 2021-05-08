2bb7f722f5b35949cdcd8864ed23c537e9853e8b
wuyanchang <1966912945@qq.com>
2021-05-08 11:21:23 +0800

    main

---
+++ src/main/java/designPattern/main.java
@@ -0,0 +1,14 @@
+package designPattern;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2021/5/8 11:20
+ */
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("design pattern");
+    }
+}
