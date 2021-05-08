f04c86c0d2cf298a387cef03dd80469f87c101c2
wuyanchang <1966912945@qq.com>
2021-05-08 11:11:29 +0800

    test

---
+++ src/test/java/com/ops/designpattern/test/test.java
@@ -0,0 +1,14 @@
+package com.ops.designpattern.test;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2021/5/8 11:09
+ */
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
