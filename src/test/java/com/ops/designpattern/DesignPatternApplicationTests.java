5608e959d25d1ab10cc4742b9687a0bdf141dccd
wuyanchang <1966912945@qq.com>
2021-05-08 10:08:53 +0800

    first commit

---
+++ src/test/java/com/ops/designpattern/DesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ops.designpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
