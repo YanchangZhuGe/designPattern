805271f0110e4e5366e5ab13b203268d1bd0e8a2
wuyanchang <1966912945@qq.com>
2021-05-11 15:22:53 +0800

    单例模式6

---
+++ src/main/java/designPattern/creativion/singleton/SingletonExample6.java
@@ -0,0 +1,24 @@
+package designPattern.creativion.singleton;
+
+/**
+ * 描述:饿汉模式，单例实例在类装载的时候进行创建，是线程安全的
+ *
+ * @author WuYanchang
+ * @date 2021/5/11 15:20
+ */
+
+public class SingletonExample6 {
+    private SingletonExample6() {
+    }
+
+    private static SingletonExample6 instance = null;
+
+    static {
+        instance = new SingletonExample6();
+    }
+
+    public static SingletonExample6 getInstance() {
+        return instance;
+    }
+
+}
