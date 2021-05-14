b130897bf9aad2a0ff2ef90de35532d51c85c1b6
wuyanchang <1966912945@qq.com>
2021-05-14 14:50:15 +0800

    servlet

---
+++ src/main/java/designPattern/servlet/servletConnection.java
@@ -0,0 +1,42 @@
+package designPattern.servlet;
+
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2021/5/14 14:38
+ */
+
+public class servletConnection {
+    public static void main(String[] args) {
+        HttpURLConnection servletConnection = null;
+        URL uploadServlet = null;
+
+        try {
+            uploadServlet = new URL("http://192.168.1.192:8082/SjsbStatusHttpServlet");
+            servletConnection = (HttpURLConnection)uploadServlet.openConnection();
+            servletConnection.setConnectTimeout(10000);
+            servletConnection.setReadTimeout(10000);
+            servletConnection.setRequestMethod("POST");
+            servletConnection.setDoOutput(true);
+            servletConnection.setDoInput(true);
+            servletConnection.setAllowUserInteraction(true);
+
+            OutputStream output = servletConnection.getOutputStream();
+//            encode = this.util.buildStatusRequestXML(ipConfigMap);
+//            String zipStr = this.util.zip(encode);
+//            output.write(zipStr.getBytes("GBK"));
+            output.flush();
+            output.close();
+//            responseCode = servletConnection.getResponseCode();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
