ba438d251435d16108855bbc9fec80340dd60cfa
wuyanchang <1966912945@qq.com>
2021-06-23 16:25:00 +0800

    时间工具

--- src/main/java/designPattern/other/base/Regex.java
+++ src/main/java/designPattern/other/base/Regex.java
@@ -1,5 +1,6 @@
 package designPattern.other.base;
 
+import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -21,9 +22,10 @@ public class Regex {
         System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
 
         testMatcher();
+        getData();
     }
 
-    public static void testMatcher(){
+    public static void testMatcher() {
         // 按指定模式在字符串查找
         String line = "This order was placed for QT3000! OK?";
         String pattern = "(\\D*)(\\d+)(.*)";
@@ -33,13 +35,31 @@ public class Regex {
 
         // 现在创建 matcher 对象
         Matcher m = r.matcher(line);
-        if (m.find( )) {
-            System.out.println("Found value: " + m.group(0) );
-            System.out.println("Found value: " + m.group(1) );
-            System.out.println("Found value: " + m.group(2) );
-            System.out.println("Found value: " + m.group(3) );
+        if (m.find()) {
+            System.out.println("Found value: " + m.group(0));
+            System.out.println("Found value: " + m.group(1));
+            System.out.println("Found value: " + m.group(2));
+            System.out.println("Found value: " + m.group(3));
         } else {
             System.out.println("NO MATCH");
         }
+    }
+
+    public static void getData() {
+        // 初始化 Date 对象
+        Date date = new Date();
+
+        //c的使用
+        System.out.printf("全部日期和时间信息：%tc%n", date);
+        //f的使用
+        System.out.printf("年-月-日格式：%tF%n", date);
+        //d的使用
+        System.out.printf("月/日/年格式：%tD%n", date);
+        //r的使用
+        System.out.printf("HH:MM:SS PM格式（12时制）：%tr%n", date);
+        //t的使用
+        System.out.printf("HH:MM:SS格式（24时制）：%tT%n", date);
+        //R的使用
+        System.out.printf("HH:MM格式（24时制）：%tR", date);
     }
 }
