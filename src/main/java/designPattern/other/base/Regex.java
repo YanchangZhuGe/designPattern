8cfd82b3468156dbac45b00e48c58e0e68e86742
wuyanchang <1966912945@qq.com>
2021-06-23 16:19:11 +0800

    从一个给定的字符串中找到数字串

--- src/main/java/designPattern/other/base/Regex.java
+++ src/main/java/designPattern/other/base/Regex.java
@@ -1,5 +1,6 @@
 package designPattern.other.base;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
@@ -18,5 +19,27 @@ public class Regex {
 
         boolean isMatch = Pattern.matches(pattern, content);
         System.out.println("字符串中是否包含了 'runoob' 子字符串? " + isMatch);
+
+        testMatcher();
+    }
+
+    public static void testMatcher(){
+        // 按指定模式在字符串查找
+        String line = "This order was placed for QT3000! OK?";
+        String pattern = "(\\D*)(\\d+)(.*)";
+
+        // 创建 Pattern 对象
+        Pattern r = Pattern.compile(pattern);
+
+        // 现在创建 matcher 对象
+        Matcher m = r.matcher(line);
+        if (m.find( )) {
+            System.out.println("Found value: " + m.group(0) );
+            System.out.println("Found value: " + m.group(1) );
+            System.out.println("Found value: " + m.group(2) );
+            System.out.println("Found value: " + m.group(3) );
+        } else {
+            System.out.println("NO MATCH");
+        }
     }
 }
