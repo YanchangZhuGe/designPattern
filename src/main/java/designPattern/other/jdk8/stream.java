d6429a9677af65f2d333872daf940adc0409338e
wuyanchang <1966912945@qq.com>
2021-07-02 14:42:56 +0800

    数字平方排序（倒叙）输出。字符串转 map 输出。

--- src/main/java/designPattern/other/jdk8/stream.java
+++ src/main/java/designPattern/other/jdk8/stream.java
@@ -189,3 +189,33 @@ public class stream {
         return getSum(numbers) / numbers.size();
     }
 }
+
+//数字平方排序（倒叙）输出。字符串转 map 输出。
+
+class Main {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(3, 2, 2, 3, 7, 3, 5);
+        // 获取对应的平方数
+//        List<Integer> squaresList = numbers.stream().map( i -> i*i).distinct().collect(Collectors.toList());
+        List<Integer> squaresList = numbers.stream()
+                .map(i -> i * i)
+                .sorted((x, y) -> y - x)
+                .collect(Collectors.toList());
+//        squaresList.forEach(System.out::println);
+        squaresList.forEach(num -> {
+            num++;
+            System.out.println(num);
+        });
+
+        List<String> strList = Arrays.asList("a", "ba", "bb", "abc", "cbb", "bba", "cab");
+        Map<Integer, String> strMap = new HashMap<Integer, String>();
+
+        strMap = strList.stream()
+                .collect(Collectors.toMap(str -> strList.indexOf(str), str -> str));
+
+        strMap.forEach((key, value) -> {
+            System.out.println(key + "::" + value);
+        });
+
+    }
+}
