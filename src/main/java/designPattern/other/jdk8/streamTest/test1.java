3e771ce0a069a4abd3e28f448b75a9ac4007d5d4
wuyanchang <1966912945@qq.com>
2021-07-16 09:52:14 +0800

    stream

--- src/main/java/designPattern/other/jdk8/streamTest/test1.java
+++ src/main/java/designPattern/other/jdk8/streamTest/test1.java
@@ -2,6 +2,7 @@ package designPattern.other.jdk8.streamTest;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.function.Predicate;
 
 /**
@@ -37,6 +38,15 @@ public class test1 {
 
         System.out.println("输出大于 3 的所有数字:");
         eval(list, n -> n > 3);
+
+        test1 java8Tester = new test1();
+        Integer value1 = null;
+        Integer value2 = new Integer(10);
+        // Optional.ofNullable - 允许传递为 null 参数
+        Optional<Integer> a = Optional.ofNullable(value1);
+        // Optional.of - 如果传递的参数是 null，抛出异常 NullPointerException
+        Optional<Integer> b = Optional.of(value2);
+        System.out.println(java8Tester.sum(a, b));
     }
 
     public static void eval(List<Integer> list, Predicate<Integer> predicate) {
@@ -46,5 +56,18 @@ public class test1 {
                 System.out.println(n + " ");
             }
         }
+    }
+
+
+
+    public Integer sum(Optional<Integer> a, Optional<Integer> b) {
+        // Optional.isPresent - 判断值是否存在
+        System.out.println("第一个参数值存在: " + a.isPresent());
+        System.out.println("第二个参数值存在: " + b.isPresent());
+        // Optional.orElse - 如果值存在，返回它，否则返回默认值
+        Integer value1 = a.orElse(new Integer(0));
+        //Optional.get - 获取值，值需要存在
+        Integer value2 = b.get();
+        return value1 + value2;
     }
 }
