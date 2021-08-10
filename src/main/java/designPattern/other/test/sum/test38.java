27ec320580a06e9d8fc781e0d15b8eb372c73f4d
wuyanchang <1966912945@qq.com>
2021-08-10 19:31:14 +0800

    写一个函数，求一个字符串的长度，

---
+++ src/main/java/designPattern/other/test/sum/test38.java
@@ -0,0 +1,18 @@
+package designPattern.other.test.sum;
+
+import java.util.Scanner;
+
+/**
+ * 描述:写一个函数，求一个字符串的长度，
+ *
+ * @author WuYanchang
+ * @date 2021/8/10 19:30
+ */
+
+public class test38 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String toString = input.nextLine();
+        System.out.println(toString.length());
+    }
+}
