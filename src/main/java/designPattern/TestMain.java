0d37464b653b7aa46cb746db7f213d0d7292e2aa
wuyanchang <1966912945@qq.com>
2022-03-07 14:26:03 +0800

    首页

--- src/main/java/designPattern/TestMain.java
+++ src/main/java/designPattern/TestMain.java
@@ -1,24 +1,71 @@
 package designPattern;
 
-import java.util.Date;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
- * 描述: 测试
+ * 描述:
  *
  * @author WuYanchang
- * @date 2021/5/8 11:20
+ * @date 2021/5/8 11:09
  */
 
 public class TestMain {
     public static void main(String[] args) {
+        TreeMap<Person, String> pdata = new TreeMap<Person, String>();
+        pdata.put(new Person("张三", 30), "zhangsan");
+        pdata.put(new Person("李四", 20), "lisi");
+        pdata.put(new Person("王五", 10), "wangwu");
+        pdata.put(new Person("小红", 5), "xiaohong");
+        // 得到key的值的同时得到key所对应的值
+        Set<Person> keys = pdata.keySet();
+        for (Person key : keys) {
+            System.out.println(key.getAge() + "-" + key.getName());
 
-//        SingletonExample1 singletonExample1 = new SingletonExample1();
-        Date fx_date_d = new Date( );
+        }
+    }
+}
 
-        int i = (int) (Math.random() * 5) +2;
-        System.out.println(i);
-        System.out.println(fx_date_d);
+// person对象没有实现Comparable接口，所以必须实现，这样才不会出错，才可以使treemap中的数据按顺序排列
+// 前面一个例子的String类已经默认实现了Comparable接口，详细可以查看String类的API文档，另外其他
+// 像Integer类等都已经实现了Comparable接口，所以不需要另外实现了
+class Person implements Comparable<Person> {
+    private String name;
+    private int age;
 
+    public Person(String name, int age) {
+        super();
+        this.name = name;
+        this.age = age;
+    }
 
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    /**
+     * T重写compareTo方法实现按年龄来排序
+     */
+    @Override
+    public int compareTo(Person o) {
+        if (this.age > o.getAge()) {
+            return 1;
+        }
+        if (this.age < o.getAge()) {
+            return -1;
+        }
+        return 0;
     }
 }
