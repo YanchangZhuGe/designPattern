7458bad923907deefd822c720d7f19f2f487f906
zhugeyanchang <1966912945@qq.com>
2022-04-22 16:10:36 +0800

    admin

---
+++ admin/src/main/java/com/ops/Main.java
@@ -0,0 +1,13 @@
+package com.ops;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
