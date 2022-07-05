97236ecb51d8a0a34869f1846e54db0ea2455af4
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-05 09:30:45 +0800

    mapper

---
+++ mybatis-plus/src/main/java/com/ops/web/mapper/AimsAssociatebankMapper.java
@@ -0,0 +1,16 @@
+package com.ops.web.mapper;
+
+import com.ops.web.entity.AimsAssociatebankDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 合作金融机构登记表 Mapper 接口
+ * </p>
+ *
+ * @author wyc
+ * @since 2022-07-05
+ */
+public interface AimsAssociatebankMapper extends BaseMapper<AimsAssociatebankDO> {
+
+}
