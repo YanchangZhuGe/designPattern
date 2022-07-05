39b81bd9a84f2ba2f9d3c64a99abb74fb9fe4967
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-05 09:30:54 +0800

    service

---
+++ mybatis-plus/src/main/java/com/ops/web/service/IAimsAssociatebankService.java
@@ -0,0 +1,16 @@
+package com.ops.web.service;
+
+import com.ops.web.entity.AimsAssociatebankDO;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ * 合作金融机构登记表 服务类
+ * </p>
+ *
+ * @author wyc
+ * @since 2022-07-05
+ */
+public interface IAimsAssociatebankService extends IService<AimsAssociatebankDO> {
+
+}
