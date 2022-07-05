e497a99d8a2fb56d05f6007b0d3d32c9d7f637f1
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-05 09:31:06 +0800

    AimsAssociatebankDO

---
+++ mybatis-plus/src/main/java/com/ops/web/service/impl/AimsAssociatebankServiceImpl.java
@@ -0,0 +1,20 @@
+package com.ops.web.service.impl;
+
+import com.ops.web.entity.AimsAssociatebankDO;
+import com.ops.web.mapper.AimsAssociatebankMapper;
+import com.ops.web.service.IAimsAssociatebankService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 合作金融机构登记表 服务实现类
+ * </p>
+ *
+ * @author wyc
+ * @since 2022-07-05
+ */
+@Service
+public class AimsAssociatebankServiceImpl extends ServiceImpl<AimsAssociatebankMapper, AimsAssociatebankDO> implements IAimsAssociatebankService {
+
+}
