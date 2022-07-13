ff7e10827d5481a3f70c17afaf8d4771dd9a57d5
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-13 09:32:13 +0800

    BussLedgerType

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/BussLedgerTypeMapper.java
@@ -0,0 +1,16 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.BussLedgerType;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author chenyuhao
+ * @since 2021-04-12
+ */
+public interface BussLedgerTypeMapper extends BaseMapper<BussLedgerType> {
+
+}
