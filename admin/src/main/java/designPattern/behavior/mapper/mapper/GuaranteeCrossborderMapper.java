12dd79579f4ea2b7ddbfb24c495f635fdb95f29a
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-14 09:24:38 +0800

    entity

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/GuaranteeCrossborderMapper.java
@@ -0,0 +1,20 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.GuaranteeCrossborder;
+import org.apache.ibatis.annotations.Param;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author chenyuhao
+ * @since 2021-04-12
+ */
+public interface GuaranteeCrossborderMapper extends BaseMapper<GuaranteeCrossborder> {
+
+    void updateGuaranteeByNoRecord(@Param("scope") GuaranteeCrossborder guarantee);
+
+    void updateGuaranteeByRecord(@Param("scope") GuaranteeCrossborder guarantee);
+}
