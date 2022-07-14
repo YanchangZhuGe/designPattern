0aa0874d3214fd4d2dda7deb2e43f7e9045c2922
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-14 09:24:45 +0800

    entity

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/GuaranteeCounterMapper.java
@@ -0,0 +1,16 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.GuaranteeCounter;
+
+/**
+ * <p>
+ * Mapper 接口
+ * </p>
+ *
+ * @author chenyuhao
+ * @since 2021-04-12
+ */
+public interface GuaranteeCounterMapper extends BaseMapper<GuaranteeCounter> {
+
+}
