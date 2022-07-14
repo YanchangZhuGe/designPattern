c781b5c4d76ce7a9dbd4c12251cbf48967690078
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-14 09:23:53 +0800

    DeductionPool

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/DeductionPoolMapper.java
@@ -0,0 +1,16 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.DeductionPool;
+
+/**
+ * <p>
+ * 担保费用抵扣池 Mapper 接口
+ * </p>
+ *
+ * @author chenyuhao
+ * @since 2021-04-12
+ */
+public interface DeductionPoolMapper extends BaseMapper<DeductionPool> {
+
+}
