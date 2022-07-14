b85e2ac04772cbc9785c73920c44cec225476588
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-14 09:24:20 +0800

    entity

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/FeesRecMapper.java
@@ -0,0 +1,16 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.FeesRec;
+
+/**
+ * <p>
+ * 融资还本，抵扣池增加记录 Mapper 接口
+ * </p>
+ *
+ * @author chenyuhao
+ * @since 2021-04-12
+ */
+public interface FeesRecMapper extends BaseMapper<FeesRec> {
+
+}
