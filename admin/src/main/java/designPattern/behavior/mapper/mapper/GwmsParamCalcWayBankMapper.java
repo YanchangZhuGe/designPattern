7b98fc214abab9939d63b4a2010fd2fcf00be62b
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-15 13:46:59 +0800

    default

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/GwmsParamCalcWayBankMapper.java
@@ -0,0 +1,7 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.GwmsParamCalcWayBank;
+
+public interface GwmsParamCalcWayBankMapper extends BaseMapper<GwmsParamCalcWayBank> {
+}
