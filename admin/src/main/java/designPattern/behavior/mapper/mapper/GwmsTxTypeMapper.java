71cb7287719aaeb045ce34e98046418b38a0283a
wuyanchang <wuyanchang@nstc.com.cn>
2022-07-18 13:46:37 +0800

    default

---
+++ admin/src/main/java/designPattern/behavior/mapper/mapper/GwmsTxTypeMapper.java
@@ -0,0 +1,16 @@
+package designPattern.behavior.mapper.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.nstc.gwms.entity.TxType;
+
+/**
+ * <p>
+ * 交易类型映射 Mapper 接口
+ * </p>
+ *
+ * @author wk
+ * @since 2021-1-12
+ */
+public interface GwmsTxTypeMapper extends BaseMapper<TxType> {
+
+}
