9aefbc27c2160d9c0e8ca1ebac9738327f184440
吴延昌 <wuyanchang_mail@qq.com>
2022-08-31 09:20:27 +0800

    整理

---
+++ admin/src/main/java/com/bgd/api/common/constants/BizFlowConstants.java
@@ -0,0 +1,93 @@
+/**
+ *
+ */
+package com.bgd.api.common.constants;
+
+/**
+ * <p>Title: </p>
+ *
+ * <p>Description: </p>
+ *
+ * <p>Company: 北京九恒星科技股份有限公司</p>
+ *
+ * @author zhanghonghui
+ *
+ * @since：2017-9-7 上午09:56:07
+ *
+ */
+public class BizFlowConstants {
+
+	public static final String BIZ_FLOW_ACTION = "bizFlowAction";
+
+	//工作流公共单元名称
+	public static final String FLOW_COMMON_UNIT = "";
+	/**
+	 * 工作流任务编码
+	 */
+	public static final String TASK_KEY = "taskKey";
+	/**
+	 * 工作流编码
+	 */
+	public static final String FLOW_KEY = "flowKey";
+	/**
+	 * 工作流Id
+	 */
+	public static final String CASEID = "caseId";
+	/**
+	 * 工单id
+	 */
+	public static final String FMID = "fmId";
+	/**
+	 * 工作流审批类型
+	 */
+	public static final String EXEC_TYPE = "execType";
+	/**
+	 * 审批意见
+	 */
+	public static final String ATTITUDE = "attitude";
+	/**
+	 * 工作流审批caseid拼接字符串
+	 */
+	public static final String CASEIDS = "caseIds";
+	/**
+	 * 工作流对象
+	 */
+	public static final String BIZCASE = "bizCase";
+	/**
+	 * 返回结果
+	 */
+	public static final String RESULT = "result";
+	/**
+	 * 工作流名称
+	 */
+	public static final String FLOW_NAME = "flowName";
+	/**
+	 * 工作流备注
+	 */
+	public static final String DESCRIPTION = "description";
+	/**
+	 * 工作流审批结果信息
+	 */
+	public static final String RESULT_INFO = "resultInfo";
+	/**
+	 * 返回页面url的key
+	 */
+	public static final String BACK_URL_KEY = "backUrl";
+	/**
+	 * 返回页面url
+	 */
+	public static final String BACK_URL = "";
+	/**
+	 * 工单类型
+	 */
+	public static final String FMCLS = "fmCls";
+
+	/**
+	 * GWMS
+	 */
+	public static final String GWMS = "GWMS";
+	/**
+	 * FLOW
+	 */
+	public static final String FLOW = "FLOW";
+}
