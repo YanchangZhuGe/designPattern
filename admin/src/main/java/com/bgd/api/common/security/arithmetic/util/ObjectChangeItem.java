e2b97a7abdb04fdfb19e3cd9c58a71ed4ea252e4
吴延昌 <wuyanchang_mail@qq.com>
2022-08-31 09:22:43 +0800

    default

---
+++ admin/src/main/java/com/bgd/api/common/security/arithmetic/util/ObjectChangeItem.java
@@ -0,0 +1,67 @@
+package com.bgd.api.common.security.arithmetic.util;
+
+public class ObjectChangeItem {
+	private String fieldNameText;
+	private String fieldName;
+	private String orgVal;
+	private String newVal;
+
+
+	/**
+	 * @return String
+	 */
+	public String getFieldNameText() {
+		return fieldNameText;
+	}
+
+	/**
+	 * @param fieldNameText String
+	 */
+	public void setFieldNameText(String fieldNameText) {
+		this.fieldNameText = fieldNameText;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getFieldName() {
+		return fieldName;
+	}
+
+	/**
+	 * @param fieldName String
+	 */
+	public void setFieldName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getOrgVal() {
+		return orgVal;
+	}
+
+	/**
+	 * @param orgVal String
+	 */
+	public void setOrgVal(String orgVal) {
+		this.orgVal = orgVal;
+	}
+
+	/**
+	 * @return String
+	 */
+	public String getNewVal() {
+		return newVal;
+	}
+
+	/**
+	 * @param newVal String
+	 */
+	public void setNewVal(String newVal) {
+		this.newVal = newVal;
+	}
+
+
+}
