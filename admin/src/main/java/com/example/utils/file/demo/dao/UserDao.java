197a861f5b2ef6ecc553607103d3af6c6276470d
zhugeyanchang <wuyanchang_mail@qq.com>
2023-12-25 00:20:59 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/demo/dao/UserDao.java
@@ -0,0 +1,16 @@
+package com.example.utils.file.demo.dao;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.example.utils.file.demo.entity.User;
+
+import javax.annotation.Resource;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2022/4/8 10:11
+ */
+@Resource
+public interface UserDao extends BaseMapper<User> {
+}
