1663c973cde437c063a68a647dc7c724832abcda
zhugeyanchang <wuyanchang_mail@qq.com>
2023-12-26 22:09:34 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/demo/job/DemoJob.java
@@ -0,0 +1,32 @@
+package com.example.utils.file.demo.job;
+
+import com.nstc.nsosp.job.core.context.NSospJobHelper;
+import com.nstc.nsosp.job.core.handler.annotation.NSospJob;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2022/4/11 12:00
+ */
+
+@Configuration
+public class DemoJob {
+
+    @NSospJob(jobId = "demoJobHandler",jobName = "demoJobHandler",scheduMode = "2",jobCron = "0/2 * * * * ? *"
+            ,moduleName = "wuyc-job")
+    public void demoJobHandler() throws Exception {
+        NSospJobHelper.log("XXL-JOB, Hello World.");
+        System.out.println("XXL-JOB, Hello World.");
+
+        for (int i = 0; i < 5; i++) {
+            NSospJobHelper.log("beat at:" + i);
+            System.out.println("beat at:" + i);
+            TimeUnit.SECONDS.sleep(2);
+        }
+        // default success
+    }
+}
