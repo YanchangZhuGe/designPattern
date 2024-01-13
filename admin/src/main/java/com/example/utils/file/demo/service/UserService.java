afdc8406f80737cd588f9d7dccdd16fcaf6e2375
zhugeyanchang <wuyanchang_mail@qq.com>
2024-01-13 23:39:14 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/demo/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.utils.file.demo.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.example.utils.file.demo.entity.User;
+import org.springframework.stereotype.Service;
+
+/**
+ * 描述:
+ *
+ * @author WuYanchang
+ * @date 2022/4/8 10:14
+ */
+@Service
+public interface UserService extends IService<User> {
+}
