5cdde8916c7e7a8f91fa2146380ab9d414c532c6
zhugeyanchang <wuyanchang_mail@qq.com>
2024-02-14 21:18:23 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/enums/DisplayTypeEnum.java
@@ -0,0 +1,40 @@
+package com.example.utils.file.enums;
+
+/**
+ * <p>Title：</p>
+ *
+ * <p>Description： 内部评测项目设置，显示方式枚举</p>
+ *
+ * <p>Company：北京九恒星科技股份有限公司</p>
+ *
+ * @author xiongyong
+ * 
+ * @since：2018年2月12日 上午10:52:15
+ * 
+ */
+public enum DisplayTypeEnum implements TypeEnum {
+
+	SUMMARIZED_ITEM(0,"汇总项"),
+	DIVIDE_ITEM(1,"打分项");
+	
+	private Integer value;
+	private String name;
+	public Integer getValue() {
+		return value;
+	}
+	private DisplayTypeEnum(Integer value, String name) {
+		this.value = value;
+		this.name = name;
+	}
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
