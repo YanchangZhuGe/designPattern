54ac45229b096e652f74b997d99a65ba8028eaa1
zhugeyanchang <wuyanchang_mail@qq.com>
2024-02-27 21:37:29 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/enums/DumpType.java
@@ -0,0 +1,71 @@
+/*================================================================================
+* DumpType.java
+* 生成日期：2015-9-25 上午11:05:14 
+* 作          者：zhanghonghui
+* 项          目：GWMS-Service
+* (C)COPYRIGHT BY NSTC.
+* ================================================================================
+*/
+package com.example.utils.file.enums;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * 
+ * <p>
+ * Description:转存方式
+ * </p>
+ * 
+ * <p>
+ * Company: 北京九恒星科技股份有限公司
+ * </p>
+ * 
+ * @author zhanghonghui
+ * 
+ * @since：2015-9-25 上午11:05:14
+ * 
+ * @version 1.0
+ */
+public enum DumpType implements TypeEnum {
+	/**
+	 * 本金转存
+	 */
+	PRINCIAL("01","本金转存"),
+	/**
+	 * 本息转存
+	 */
+	PRINCIAL_INTEREST("02","本息转存");
+	private String value;
+	private String name;
+	DumpType(String value,String name){
+		this.value = value;
+		this.name = name;
+	}
+	/**
+	 * @return String
+	 */
+	public String getValue() {
+		return value;
+	}
+	/**
+	 * @param value String
+	 */
+	public void setValue(String value) {
+		this.value = value;
+	}
+	/**
+	 * @return String
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name String
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
