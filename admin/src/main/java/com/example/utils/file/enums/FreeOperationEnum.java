6fc3e6789a7e899e1d1c5d2ec79241e8361179bc
zhugeyanchang <wuyanchang_mail@qq.com>
2024-02-29 21:25:04 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/enums/FreeOperationEnum.java
@@ -0,0 +1,69 @@
+package com.example.utils.file.enums;
+
+/**
+ * 
+ * <p>
+ * Title:
+ * </p>
+ * 
+ * <p>
+ * Description:免收操作
+ * </p>
+ * 
+ * <p>
+ * Company: 北京九恒星科技股份有限公司
+ * </p>
+ * 
+ * @author lj
+ * 
+ * @since：2018年2月27日 下午3:21:11
+ * 
+ * @version 1.0
+ */
+public enum FreeOperationEnum implements TypeEnum {
+    
+	/** 无*/
+    FREE_NONE(0,"无"),
+	/** 免收滞纳金 */
+	FREE_LATEFEE(1,"免收滞纳金"),
+	/** 免收全部*/
+	FREE_ALL(2,"免收全部");
+	
+	private Integer value;
+	
+	private String name;
+
+	FreeOperationEnum(Integer value, String name){
+		this.value = value;
+		this.name = name;
+	}
+	
+	
+	public static FreeOperationEnum getFreeOperationEnum(Integer value){
+        for(FreeOperationEnum p :values()){
+            if(p.getValue().equals(value)){
+                return p;
+            }
+        }
+        return null;
+    }
+	
+	public Integer getValue() {
+		return value;
+	}
+
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
