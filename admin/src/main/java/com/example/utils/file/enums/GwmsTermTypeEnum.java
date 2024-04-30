db5ac6037c8896a70e25f0511c06ae1d34f7be49
zhugeyanchang <wuyanchang_mail@qq.com>
2024-04-30 22:16:08 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/enums/GwmsTermTypeEnum.java
@@ -0,0 +1,52 @@
+package com.example.utils.file.enums;
+
+/**
+ * 期限，1日，2月，3年
+ */
+public enum GwmsTermTypeEnum implements TypeEnum {
+
+    /** 年 */
+    YEAR("3", "年"),
+    /** 天 */
+    MONTH("2", "月"),
+    /** 天 */
+    DAY("1", "日")
+    ;
+
+    private String value;
+
+    private String name;
+
+    GwmsTermTypeEnum(String value, String name) {
+        this.value = value;
+        this.name = name;
+    }
+
+    public static String getNameByValue(String value) {
+        if (value == null) {
+            return null;
+        }
+
+        for (GwmsTermTypeEnum temp : values()) {
+            if (temp == null) {
+                continue;
+            }
+
+            if (value.equals(temp.getValue())) {
+                return temp.getName();
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public String getValue() {
+        return value;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+}
