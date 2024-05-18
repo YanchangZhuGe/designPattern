70d7d24424aa3a1915a4484a7960ccae835bf3ce
zhugeyanchang <wuyanchang_mail@qq.com>
2024-05-18 12:08:09 +0800

    controller

---
+++ admin/src/main/java/com/example/utils/file/enums/PledgeTypeEnum.java
@@ -0,0 +1,88 @@
+/*================================================================================
+* DumpType.java
+* 生成日期：2015-9-25 上午11:05:14 
+* 作          者：zhanghonghui
+* 项          目：GWMS-Service
+* (C)COPYRIGHT BY NSTC.
+* ================================================================================
+*/
+package com.example.utils.file.enums;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * 
+ * <p>
+ * Description:抵质押类型
+ * </p>
+ * 
+ * <p>
+ * Company: 北京九恒星科技股份有限公司
+ * </p>
+ * 
+ * @author zhanghonghui
+ * 
+ * @since：2015-9-25 上午11:05:14
+ * 
+ * @version 1.0
+ */
+public enum PledgeTypeEnum implements TypeEnum {
+	/**
+	 * 抵押 1
+	 */
+	MORTGAGE(new Integer(1),"抵押"),
+	/**
+	 * 质押 2
+	 */
+	PLEDGE(new Integer(2),"质押");
+	private Integer value;
+	private String name;
+	PledgeTypeEnum(Integer value,String name){
+		this.value = value;
+		this.name = name;
+	}
+	
+	/**
+	 * 
+	 * @Description:
+	 * @param value
+	 * @return
+	 * @author zhanghonghui
+	 * @since 2015-9-22 上午11:05:10
+	 */
+	public static String getTypeName(Integer value){
+		for(PledgeTypeEnum t : values()){
+			if(t.getValue().equals(value)){
+				return t.getName();
+			}
+		}
+		return "";
+	}
+	/**
+	 * @return String
+	 */
+	public Integer getValue() {
+		return value;
+	}
+	/**
+	 * @param value String
+	 */
+	public void setValue(Integer value) {
+		this.value = value;
+	}
+	/**
+	 * @return String
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name String
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
